50da1d4f63dfd87d177b52fd31c440c4c63a6268
Sahil1517 <sahilsumandbg2002@gmail.com>
2023-02-04 15:05:19 +0530

    FirstCommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+import React from "react";
+import NavBar from "./components/NavBar";
+import News from "./components/News";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <NavBar />
+        <Routes>
+           <Route
+            exact
+            path="/home"
+            element={<News key="general" country="in" category="general" />}
+          /> 
+           <Route
+            exact
+            path="/newsapp"
+            element={<News key="general" country="in" category="general" />}
+          />
+           <Route 
+            exact
+            path="/sports"
+            element={<News key="sports" country="in" category="sports" />}
+          />
+          <Route
+            exact
+            path="/science"
+            element={<News key="science" country="in" category="science" />}
+          />
+          <Route
+            exact
+            path="/entertainment"
+            element={
+              <News key="entertainment" country="in" category="entertainment" />
+            }
+          />
+
+          <Route
+            exact
+            path="/technology"
+            element={
+              <News key="technology" country="in" category="technology" />
+            }
+          />
+          <Route
+            exact
+            path="/health"
+            element={<News key="health" country="in" category="health" />}
+          />
+        </Routes>
+      </Router>
     </div>
   );
 }
-
-export default App;
